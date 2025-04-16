e25070a29705c4f4952aee5df69ab8c502c3389c
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-15 22:26:54 -0400

    feat: create App component to integrate Home, About, Projects, Contact, and Footer

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from './components/Footer'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
