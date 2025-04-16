038cf457ab417a1d17f5fd0886eea8d2428e023d
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-15 22:28:36 -0400

    feat: add Projects component to showcase featured projects with details

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+function Projects() {
+  return (
+<div className="hero  min-h-screen">
+  <div className="hero-content text-center">
+    <div className="max-w-6xl">
+      <h1 className="text-5xl font-bold gradient-text">Featured Projects</h1>
+      
+      <div className='grid grid-cols-1 gap-4 py-6 md:grid-cols-3'>
+      <div className="card w-full bg-base-300 card-sm shadow-sm">
+            <div className="card-body">
+                <h2 className="card-title">E-commerce Backend</h2>
+                <p className='text-left'>API para productos, carritos y pagos, integrando Stripe para compras seguras.</p>
+                <div className="flex gap-2 flex-wrap">
+                    <div className="badge badge-soft badge-info">Node.js</div>
+                    <div className="badge badge-soft badge-info">Express</div>
+                    <div className="badge badge-soft badge-info">MongoDB</div>
+                </div>
+                <div className="card-actions justify-end py-2">
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-arrow-right-s-line text-xl"></i></button>
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-github-fill text-xl"></i></button>
+                </div>
+            </div>
+        </div>
+        <div className="card w-full bg-base-300 card-sm shadow-sm">
+            <div className="card-body">
+                <h2 className="card-title">E-commerce Backend</h2>
+                <p className='text-left'>API para productos, carritos y pagos, integrando Stripe para compras seguras.</p>
+                <div className="flex gap-2 flex-wrap">
+                    <div className="badge badge-soft badge-info">Node.js</div>
+                    <div className="badge badge-soft badge-info">Express</div>
+                    <div className="badge badge-soft badge-info">MongoDB</div>
+                </div>
+                <div className="card-actions justify-end py-2">
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-arrow-right-s-line text-xl"></i></button>
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-github-fill text-xl"></i></button>
+                </div>
+            </div>
+        </div>
+        <div className="card w-full bg-base-300 card-sm shadow-sm">
+            <div className="card-body">
+                <h2 className="card-title">E-commerce Backend</h2>
+                <p className='text-left'>API para productos, carritos y pagos, integrando Stripe para compras seguras.</p>
+                <div className="flex gap-2 flex-wrap">
+                    <div className="badge badge-soft badge-info">Node.js</div>
+                    <div className="badge badge-soft badge-info">Express</div>
+                    <div className="badge badge-soft badge-info">MongoDB</div>
+                </div>
+                <div className="card-actions justify-end py-2">
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-arrow-right-s-line text-xl"></i></button>
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-github-fill text-xl"></i></button>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div className='grid grid-cols-1 py-4 gap-4 md:grid-cols-3'>
+      <div className="card w-full bg-base-300 card-sm shadow-sm">
+            <div className="card-body">
+                <h2 className="card-title">E-commerce Backend</h2>
+                <p className='text-left'>API para productos, carritos y pagos, integrando Stripe para compras seguras.</p>
+                <div className="flex gap-2 flex-wrap">
+                    <div className="badge badge-soft badge-info">Node.js</div>
+                    <div className="badge badge-soft badge-info">Express</div>
+                    <div className="badge badge-soft badge-info">MongoDB</div>
+                </div>
+                <div className="card-actions justify-end py-2">
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-arrow-right-s-line text-xl"></i></button>
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-github-fill text-xl"></i></button>
+                </div>
+            </div>
+        </div>
+        <div className="card w-full bg-base-300 card-sm shadow-sm">
+            <div className="card-body">
+                <h2 className="card-title">E-commerce Backend</h2>
+                <p className='text-left'>API para productos, carritos y pagos, integrando Stripe para compras seguras.</p>
+                <div className="flex gap-2 flex-wrap">
+                    <div className="badge badge-soft badge-info">Node.js</div>
+                    <div className="badge badge-soft badge-info">Express</div>
+                    <div className="badge badge-soft badge-info">MongoDB</div>
+                </div>
+                <div className="card-actions justify-end py-2">
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-arrow-right-s-line text-xl"></i></button>
+                <button className="btn btn-circle btn-outline btn-info"><i class="ri-github-fill text-xl"></i></button>
+                </div>
+            </div>
+        </div>
+
+        <div className="card w-full bg-base-300 card-sm shadow-sm">
+            <div className="card-body">
+                <h2 className="card-title">E-commerce Backend</h2>
+                <p className='text-left'>API para productos, carritos y pagos, integrando Stripe para compras seguras.</p>
+                <div className="flex gap-2 flex-wrap">
+                    <div className="badge badge-soft badge-info">Node.js</div>
+                    <div className="badge badge-soft badge-info">Express</div>
+                    <div className="badge badge-soft badge-info">MongoDB</div>
+                </div>
+                <div className="card-actions justify-end py-2">
+                    <button className="btn btn-circle btn-outline btn-info"><i class="ri-arrow-right-s-line text-xl"></i></button>
+                    <button className="btn btn-circle btn-outline btn-info"><i class="ri-github-fill text-xl"></i></button>
+                </div>
+            </div>
+        </div>
+
+      </div>
+    </div>
+    
+  </div>
+</div>
+  )
+}
+
+export default Projects
