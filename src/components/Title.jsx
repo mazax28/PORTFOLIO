c4e8c29710790710de5180424ceabe400a428556
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-15 22:27:24 -0400

    feat: add Title component for displaying page titles

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title({title}) {
+  return (
+    <div className='max-w-md text-center text-4xl'>
+        <p>{title}</p>
+    </div>
+  )
+}
+
+export default Title
