32eec553924d6c500c66b724fdbe3aba4d905e32
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-18 08:11:49 -0400

    feat: add id attribute to Home component's main div for improved styling and targeting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 function Home() {
   return (
-    <div>
+    <div id="home">
         <Navbar />
         <Hero />
     </div>
