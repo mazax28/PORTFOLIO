64cc5dfc7bbca040505d078c76091d47976a25d9
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-18 10:50:59 -0400

    feat: integrate i18next for Spanish localization support in the main application

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { I18nextProvider } from 'react-i18next'
+import i18next from 'i18next'
+import global_es from './i18n/es/global.json'
+import global_en from './i18n/en/global.json'
+
+i18next.init({
+  interpolation: {
+    escapeValue: false, // React already does escaping
+  },
+  resources:{
+    en: {
+      global: global_en,
+    },
+    es: {
+      global: global_es,
+    },
+  },
+  lng: 'es', // default language
+
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </StrictMode>,
 )
