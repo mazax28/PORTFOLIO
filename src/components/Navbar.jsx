f67ee088b6d0b65f6fb51f6e1b82d069f54d82da
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-18 21:28:54 -0400

    feat: refactor Navbar component for improved layout and styling consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,42 @@
 import { Link } from 'react-scroll';
-import {useTranslation} from 'react-i18next';
-
-
+import { useTranslation } from 'react-i18next';
 
 function Navbar() {
   const { t } = useTranslation("global");
 
-
   return (
-    <div className="navbar shadow-sm ">
-        <div className="navbar-start">
-            <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    <div className="navbar fixed top-0 left-0 w-full border-2  border-white/10 backdrop-blur-md shadow-md z-50">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <Link to='home' smooth={true} duration={500} offset={-64}><li><a>{t("navbar.home")}</a></li></Link>
+            <Link to="about" smooth={true} duration={500} offset={-64} ><li><a>{t("navbar.about")}</a></li></Link>
+            <Link to="projects" smooth={true} duration={500} offset={-64} ><li><a>{t("navbar.projects")}</a></li></Link>
+            <Link to="contact" smooth={true} duration={500} offset={-64} ><li><a>{t("navbar.contact")}</a></li></Link>
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl hidden md:block">Building The Web</a>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <Link to='home' smooth={true} duration={500} ><li><a>{t("navbar.home")}</a></li></Link>
-        <Link to="about" smooth={true} duration={500}><li><a>{t("navbar.about")}</a></li></Link>
-        <Link to="projects" smooth={true} duration={500}><li><a>{t("navbar.projects")}</a></li></Link>
-        <Link to="contact" smooth={true} duration={500}><li><a>{t("navbar.contact")}</a></li></Link>
-        
-      </ul>
-            </div>
-            <a className="btn btn-ghost text-xl">Building The Web</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-                <Link to='home' smooth={true} duration={500} ><li><a>{t("navbar.home")}</a></li></Link>
-                <Link to="about" smooth={true} duration={500}><li><a>{t("navbar.about")}</a></li></Link>
-                <Link to="projects" smooth={true} duration={500}><li><a>{t("navbar.projects")}</a></li></Link>
-                <Link to="contact" smooth={true} duration={500}><li><a>{t("navbar.contact")}</a></li></Link>
-            </ul>
-        </div>
-        <div className="navbar-end">
-            <a className="btn btn-info">{t("navbar.download")}</a>
-        </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-2">
+          <Link to='home' smooth={true} duration={500} offset={-64} ><li><a>{t("navbar.home")}</a></li></Link>
+          <Link to="about" smooth={true} duration={500} offset={-64} ><li><a>{t("navbar.about")}</a></li></Link>
+          <Link to="projects" smooth={true} duration={500} offset={-64} ><li><a>{t("navbar.projects")}</a></li></Link>
+          <Link to="contact" smooth={true} duration={500} offset={-64} ><li><a>{t("navbar.contact")}</a></li></Link>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-info">{t("navbar.download")}</a>
+      </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
