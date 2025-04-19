518d31e1421f7e51c3f0d8c53563a9de0fa083ce
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-18 21:21:43 -0400

    feat: add Toaster component for improved user notifications

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
+import { Toaster } from 'react-hot-toast';
+
 
 function App() {
 
   return (
     <>
+      <Toaster/>
       <Home />
       <About />
       <Projects />
