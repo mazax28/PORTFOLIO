c4e17cbc2b2b6a98934b9fc06188dc57b5515bad
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-18 22:20:26 -0400

    feat: add LinkedIn and GitHub links to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
   return (
     <footer className="footer footer-center  bg-white/5 backdrop-blur-md shadow-md text-white mt-16 p-6 rounded-lg">
       <nav className="flex gap-4 justify-center">
-        <a className="btn btn-circle btn-info btn-soft text-xl">
+        <a href='www.linkedin.com/in/marcos-zarate-0a77a5195' target='blank' className="btn btn-circle btn-info btn-soft text-xl">
           <i className="ri-linkedin-fill"></i>
         </a>
-        <a className="btn btn-circle btn-info btn-soft text-xl">
+        <a href='https://github.com/mazax28' target='blank' className="btn btn-circle btn-info btn-soft text-xl">
           <i className="ri-github-fill"></i>
         </a>
         <a className="btn btn-circle btn-info btn-soft text-xl">
