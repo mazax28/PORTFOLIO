9fc92d65395e42102d064914e661f356413d9cc4
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-20 13:35:06 -0400

    fix: update button styles in Footer component for improved consistency

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ function Footer() {
   return (
     <footer className="footer footer-center  bg-white/5 backdrop-blur-md shadow-md text-white mt-16 p-6 rounded-lg">
       <nav className="flex gap-4 justify-center">
-        <a href='www.linkedin.com/in/marcos-zarate-0a77a5195' target='blank' className="btn btn-circle btn-info btn-soft text-xl">
+        <a href='www.linkedin.com/in/marcos-zarate-0a77a5195' target='blank' className="btn btn-outline btn-circle btn-info text-xl">
           <i className="ri-linkedin-fill"></i>
         </a>
-        <a href='https://github.com/mazax28' target='blank' className="btn btn-circle btn-info btn-soft text-xl">
+        <a href='https://github.com/mazax28' target='blank' className="btn btn-circle btn-outline btn-info text-xl">
           <i className="ri-github-fill"></i>
         </a>
-        <a className="btn btn-circle btn-soft btn-info text-xl">
+        <a className="btn btn-circle btn-outline btn-info text-xl">
           <i className="ri-youtube-fill"></i>
         </a>
       </nav>
